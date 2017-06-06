f9a13123cbc0cb00e99fa9893daa9cacf94e71a6
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-06-06 14:48:43 +0700

    adding material-ui library

--- src/app/auth/login.js
+++ src/app/auth/login.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import {RaisedButton, MuiThemeProvider} from 'material-ui';
 
 class Login extends Component {
   render() {
     return (
-      <div>
-          Hello World
-      </div>
+      <MuiThemeProvider>
+          <RaisedButton label="Primary" primary={true} />
+      </MuiThemeProvider>
     );
   }
 }
--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { BrowserRouter as Router, Route, IndexRoute } from 'react-router-dom'
-import {Header} from "./components/Header";
+import {Header} from "./components/Header"
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 
 //Import Routes
 import Login from './auth/login'
