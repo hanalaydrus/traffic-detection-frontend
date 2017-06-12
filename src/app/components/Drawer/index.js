95c619c2a96f00890d97d3c765b44a84733e0f4c
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-06-12 16:01:29 +0700

    #39 dashboard menu

--- src/app/components/Drawer/index.js
+++ src/app/components/Drawer/index.js
@@ -3,13 +3,14 @@ import {AppBar} from 'material-ui';
 import {orange600, orange500} from 'material-ui/styles/colors';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import {DrawerMenu} from './../DrawerMenu'
 import './styles.scss';
 
 export class Drawer extends React.Component {
     render(){
     return (
       <div className={this.props.drawerStyle}>
-        
+        <DrawerMenu />
       </div>
     );
   }
---
+++ src/app/components/DrawerMenu/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {List, ListItem} from 'material-ui/List';
+
+export class DrawerMenu extends React.Component {
+
+  state = {
+    open: false,
+  };
+
+  render() {
+    return (
+      <div>
+        <br />
+          <List>
+            <ListItem primaryText="Students" />
+            <ListItem primaryText="Projects" />
+            <ListItem
+              primaryText="Curriculum"
+              initiallyOpen={false}
+              primaryTogglesNestedList={true}
+              nestedItems={[
+                <ListItem
+                  key={1}
+                  primaryText="Subject"
+                />,
+                <ListItem
+                  key={2}
+                  primaryText="Team"
+                />,
+                <ListItem
+                  key={3}
+                  primaryText="Schedule"
+                />,
+              ]}
+            />
+            <ListItem
+              primaryText="Data Master"
+              initiallyOpen={false}
+              primaryTogglesNestedList={true}
+              nestedItems={[
+                <ListItem
+                  key={1}
+                  primaryText="City"
+                />,
+                <ListItem
+                  key={2}
+                  primaryText="Campuses"
+                />,
+                <ListItem
+                  key={3}
+                  primaryText="Batches"
+                />,
+                <ListItem
+                  key={3}
+                  primaryText="Partners"
+                />,
+              ]}
+            />
+          </List>
+      </div>
+    );
+  }
+}
