38507e4034d4a329b38dd1ed93e97f6b9162f3fe
rahadian347 <mrahadian.permana@gmail.com>
2017-06-14 12:30:46 +0700

    #23 moving style to  style.scss

--- src/app/components/PreparationPic/index.js
+++ src/app/components/PreparationPic/index.js
@@ -1,49 +1,17 @@
-import React from 'react';
-import {GridList, GridTile} from 'material-ui/GridList';
-import IconButton from 'material-ui/IconButton';
-import Subheader from 'material-ui/Subheader';
-import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import React from 'react'
+import {GridList, GridTile} from 'material-ui/GridList'
+import IconButton from 'material-ui/IconButton'
+import Subheader from 'material-ui/Subheader'
+import StarBorder from 'material-ui/svg-icons/toggle/star-border'
 import {PageTitle} from '../PageTitle'
 import Preparation from '../../../../temp-data/preparationPic.json'
-import {RaisedButton, TextField, Checkbox} from 'material-ui';
-import {orange600, orange500} from 'material-ui/styles/colors';
+import {RaisedButton, TextField, Checkbox} from 'material-ui'
+import {orange600, orange500} from 'material-ui/styles/colors'
 import './styles.scss'
 
+//import dependency component
+
 import ListReview from '../ListReview'
-const styles = {
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    backgroundColor:'#33353F',
-    marginTop: 30,
-    paddingLeft: 100,
-    paddingTop: 30,
-    paddingBottom: 20
-  },
-  gridList: {
-    width: 800,
-    overflowY: 'visible',
-    backgroundColor:'#33353F',
-  },
-  review: {
-    marginBottom: 10,
-    marginRight: 10,
-    fontSize: 20,
-    fontStyle: 'italic',
-    fontWeight: 'bold',
-    height:30,
-    borderWidth: 3,
-    borderBottom: '#ff9800',
-    borderBottomStyle: 'inset',
-    backgroundColor:'#303030',
-    padding: '4px 0px 0px 8px',
-  },
-  box: {
-    marginRight: 10,
-    backgroundColor:'#33353F',
-    marginBottom: 10,
-  },
-};
 
 const prepData = Preparation.data
 
@@ -52,10 +20,10 @@ export class PreparationPic extends React.Component {
   render() {
    return (
      <div className="prep_container" style={{float:this.props.float}}>
-      <div style={styles.root}>
+      <div className='root'> 
         <GridList
           cellHeight={400}
-          style={styles.gridList}
+          className='gridList'
         >
         {prepData.map( (prep, index) => <ListReview prep={prep} />)}
         </GridList>
