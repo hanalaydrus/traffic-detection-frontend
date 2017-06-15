61f576bd7b405b5af51c3b90c07322b0322b0acc
ajilantang <ajilantang@gmail.com>
2017-06-16 02:25:23 +0700

    #14 fix student profile

--- src/app/components/StudentDetail/index.js
+++ src/app/components/StudentDetail/index.js
@@ -17,10 +17,12 @@ import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import DatePicker from 'material-ui/DatePicker';
 import Chip from 'material-ui/Chip';
-import MainMenu from '../../containers/MainMenu';
 import moment from 'moment'
-import ActionHome from 'material-ui/svg-icons/editor/mode-edit';
+import SettingIcon from 'material-ui/svg-icons/editor/mode-edit';
 import Avatar from 'material-ui/Avatar';
+
+import MainMenu from '../../containers/MainMenu';
+import response from './constants'
 const styles = {
   headline: {
     fontSize: 24,
@@ -48,8 +50,18 @@ const styles = {
  },
  chip: {
     margin: 4,
+    marginTop:4,
+    marginBottom:16
+  },
+  chip2: {
+    margin: 4,
+  },
+  wrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
   }
 };
+
 
 import './styles.scss'
 function handleActive(tab) {
@@ -60,179 +72,145 @@ export default class CardExampleControlled extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
+    this.state = Object.assign(response.data,{
       expanded: false,
       contentStyle: styles.content_less,
       controlledDate: moment('2014-11-07T21:20:15').toDate(),
-    };
+      isChange:true,
+    })
+    this.statuses = ["interview","pending","preparation","failed","discontinue","complete"]
   }
 
   handleDate = (event, date) => {
-    console.log('this is date',date)
     this.setState({
       controlledDate: date,
     });
   };
 
-  handleExpandChange = (expanded) => {
-    this.setState({expanded: expanded});
-  };
-
-  handleToggle = (event, toggle) => {
-    this.setState({expanded: toggle});
-  };
-
-  handleExpand = () => {
-    this.setState({expanded: true});
-  };
-
-  handleReduce = () => {
-    this.setState({expanded: false});
-  };
-  handleChange = (event, index, value) => this.setState({value});
-render(){
-  return (
-<MainMenu > 
-  <div style={this.state.contentStyle}> 
-  <Tabs>
-    <Tab label="Profile" >
-      <div style={styles.content} >
-        <Row around="xs" >      
-        <Col xs={6}>
-        <ToolbarGroup >
-          <TextField
-            defaultValue="Aji Lantang Mardika"
-            floatingLabelText="Name "
-          />
-          <ActionHome/>
-        </ToolbarGroup>
-         <ToolbarGroup>
-            <SelectField
-              floatingLabelText="Campus"
-              value={1}
-              onChange={this.handleChange}
-            >
-              <MenuItem value={1} primaryText="Eduplex, bandung" />
-              <MenuItem value={2} primaryText="Akademos, jakarta" />
-              <MenuItem value={3} primaryText="Cak har, surabaya" />
-              <MenuItem value={4} primaryText="Tuminem, jogja" />
-              <MenuItem value={5} primaryText="Seragen, solo" />
-          </SelectField>
-          <ActionHome/>
-         </ToolbarGroup>
-          <ToolbarGroup>
-            <SelectField
-            floatingLabelText="Batch"
-            value={1}
-            onChange={this.handleChange}
-          >
-            <MenuItem value={1} primaryText="#1-Alpacino" />
-            <MenuItem value={2} primaryText="#2-Abdul bacan" />
-            <MenuItem value={3} primaryText="#3-Azis idola" />
-          </SelectField>
-            <ActionHome />  
-          </ToolbarGroup>
-          <ToolbarGroup>
-            <DatePicker
-            hintText="Birthday"
-            value={this.state.controlledDate}
-            onChange={this.handleDate}
-          /><ActionHome/>
-          </ToolbarGroup>          
-          <ToolbarGroup>
-            <TextField
-            defaultValue="Sawangan, Depok"
-            floatingLabelText="Address "
-            />
-            <ActionHome/>
-          </ToolbarGroup>
-          <ToolbarGroup>
-            <TextField
-            defaultValue="ajilantang@gmail.com"
-            floatingLabelText="Email "
-            disabled={false}
-            />
-            <ActionHome/>
-          </ToolbarGroup>
-          
-          <p>Status</p>
-          <ToolbarGroup>
-            <Chip
-              backgroundColor={blue500}
-              style={styles.chip}
-              >
-              <Avatar size={32} color={blue500} backgroundColor={indigo900}>
-                Pe
-              </Avatar>
-              Status:Pending
-            </Chip>
-            <ActionHome/>    
-          </ToolbarGroup>
-          
-        </Col >
-      </Row>
-      </div>
-    </Tab>
-    
-  </Tabs>
-  </div>
-</MainMenu >
-  )
-}
-}
-
-/*import React, { Component } from 'react';
-import classNames from 'classnames'
-import {RaisedButton, TextField, MenuItem, SelectField, DatePicker} from 'material-ui';
-import {orange600, orange500, red500, blue500} from 'material-ui/styles/colors';
-import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn,} from 'material-ui/Table';
-import Dialog from 'material-ui/Dialog';
-import styles from "./styles.scss";
-import MainMenu from '../../containers/MainMenu'
-const customContentStyle = {
-  width: '40%',
-};
-const style = {
-  orange: {
-   borderColor: orange600,
+  onChangeValue = (state) => (event,name) => {
+      this.setState({
+        [state]:name
+      }) 
   }
-};
-const tilesData = [];
-
-class WorkToDoForm extends Component {
-  constructor(){
-    super(...arguments);
-    this.state = {
-      dataProfile: {
-                "id": 1,
-                "full_name": "Andi Herman",
-                "address": "Dago, Bandung",
-                "birthday": "May 20, 1994",
-                "phone": "08123456788",
-                "email": "andy@gmail.com",
-                "status": "pending",
-                "batch_id": 2,
-                "batch": {
-                    "id": 2,
-                    "code_name": "Basilischi",
-                    "campus_id": 1
-                },
-                "campus": {
-                    "id": 1,
-                    "name": "Eduplex",
-                }
-            }
-        
-    }
+  onSubmitStatus  = (value) =>{
+    this.setState({
+      status:value
+    })
   }
-  render() {
+  onSubmit = (event) => {
+   this.setState({
+     isChange:!this.state.isChange
+   })
+  }
+  getBatch = (data) => (campusid) => {
+  return data.filter((data) => data.campus_id === campusid )
+  }
+  handleChange = (state) => (event, index, value) => this.setState({[state]:value});
+  render(){
+    const { full_name ,batch_id, campus_id, address, birthday, phone, email, status, batch, campus ,allCampus } = this.state
+    const batches = this.getBatch(campus)(campus_id)[0]["batch"]
     return (
-      <MainMenu >
-          
-      </MainMenu>
-    );
+      <MainMenu > 
+        <div style={this.state.contentStyle}> 
+        <Tabs>
+          <Tab label="Profile" >
+            <div style={styles.content} >
+              <Row around="xs" >
+                <div >
+                  <Chip style={styles.chip2} onTouchTap={this.onSubmit}>
+                    <Avatar icon={<SettingIcon/>} />
+                    Setting information
+                  </Chip>
+                </div>
+              <Col xs={5}>
+                <TextField
+                  defaultValue={full_name}
+                  floatingLabelText="Name "
+                  onChange={this.onChangeValue('full_name')}
+                  underlineDisabledStyle={{backgroundColor:'white',fontFamily:'ubuntu'}}
+                  disabled={this.state.isChange}
+                />
+                  <SelectField
+                    floatingLabelText="Campus"
+                    value={campus_id}
+                    onChange={this.handleChange('campus_id')}
+                    underlineDisabledStyle={{backgroundColor:'white',fontFamily:'ubuntu'}}
+                    disabled={this.state.isChange}
+                  >
+                  {campus.map((data,index)=><MenuItem key={index+data} value={data.campus_id} primaryText={`${data.campus_name}`} />)}
+                </SelectField>
+                  <SelectField
+                  floatingLabelText="Batch"
+                  value={batch_id}
+                  onChange={this.handleChange('batch_id')}
+                  underlineDisabledStyle={{backgroundColor:'white',fontFamily:'ubuntu'}}
+                  disabled={this.state.isChange}
+                > 
+                  {
+                    batches.map((value,index) =><MenuItem key={value+index} value={value.id} primaryText={value.batch_name} /> )
+                  }
+                </SelectField>
+              
+                <DatePicker
+                  hintText="Birthday"
+                  value={moment(birthday).toDate()}
+                  onChange={this.onChangeValue('birthday')}
+                  underlineDisabledStyle={{backgroundColor:'white',fontFamily:'ubuntu'}}
+                  disabled={this.state.isChange}
+                />
+                <TextField
+                  defaultValue={address}
+                  floatingLabelText="Address "
+                  underlineDisabledStyle={{backgroundColor:'white',fontFamily:'ubuntu'}}
+                  onChange={this.onChangeValue('address')}
+                  disabled={this.state.isChange}
+                  />
+                <TextField
+                  defaultValue={email}
+                  floatingLabelText="Email "
+                  underlineDisabledStyle={{backgroundColor:'white',fontFamily:'ubuntu'}}
+                  onChange={this.onChangeValue('email')}
+                  disabled={this.state.isChange}
+                  />
+                  <p>Status</p>
+                {(this.state.isChange) ?
+                  <Chip
+                    backgroundColor={blue500}
+                    style={styles.chip}
+                    >
+
+                    <Avatar size={32} color={blue500} backgroundColor={indigo900}>
+                      {status.substring(0,2)}
+                    </Avatar>
+                    Status: {status}
+                  </Chip> 
+                  :
+                  <div>
+                    {this.statuses.map((statusItem,index) => (
+                      <Chip
+                        key={index+statusItem}
+                        backgroundColor={blue500}
+                        style={styles.chip}
+                        onClick={() => this.onSubmitStatus(statusItem)}
+                        >
+                        <Avatar size={32} color={blue500} backgroundColor={indigo900}>
+                          {statusItem.substring(0, 2)}
+                        </Avatar>
+                        Status: {statusItem.substring(0,1).toUpperCase() + statusItem.substring(1)}
+                      </Chip>
+                    ))}
+                  </div> 
+                }  
+                  
+              </Col >
+            </Row>
+            </div>
+          </Tab> 
+        </Tabs>
+        </div>
+      </MainMenu >
+      )
   }
 }
 
-export default WorkToDoForm;*/
 
