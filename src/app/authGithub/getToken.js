5f8a4449914dd3af5c0f290e8a6322d41a51c284
hanalaydrus <hanalaydrus@gmail.com>
2017-06-15 14:44:23 +0700

    Update for deployment

--- src/app/authGithub/getToken.js
+++ src/app/authGithub/getToken.js
@@ -1,22 +1,5 @@
 import React, { Component } from 'react'
-import axios from 'axios'
-import {
-  ENDPOINT,
-  REDIRECT_URI,
-  CLIENT_ID,
-  CLIENT_SECRET,
-  PAYLOADS,
-  CLIENT_ASSERTION_TYPE,
-  GRANT_TYPE
-} from './constants'
-
-import { browserHistory } from 'react-router'
 import { setHtmlStorage, removeHtmlStorage } from '../../helpers'
-import { authenticateUser } from './actions'
-
-const jwt = require('jsonwebtoken')
-const https = require('https')
-const Querystring = require('querystring')
 
 export default class getToken extends Component {
 
