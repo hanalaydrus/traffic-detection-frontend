70b4a7179d4598c194c8018e72102602f31aeb72
Mochamad Gufron <mgufronefendi@gmail.com>
2017-06-16 12:31:14 +0700

    update configuration for production

--- config/production.js
+++ config/production.js
@@ -1,5 +1,5 @@
 export default {
-    API_BASE_URL: 'http://academy.refactory.id/',
+    API_BASE_URL: 'http://api.refactory.id/',
   GITHUB_CLIENT_ID: 'd3606b5e42adb499b7c9',
   GITHUB_CLIENT_SECRET: '061c27a38fcd93111e62939068f503eb09f374fc',
   GITHUB_CLIENT_BASE_URI: 'github.com'
