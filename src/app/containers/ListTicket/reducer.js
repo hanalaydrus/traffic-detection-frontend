510db8fa660197674699a8e13faa524b68a0765c
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-06-16 19:08:47 +0700

    #68 header profile from API

--- src/app/containers/ListTicket/reducer.js
+++ src/app/containers/ListTicket/reducer.js
@@ -2,13 +2,15 @@ import { fromJS } from 'immutable';
 import normalize from 'jsonapi-normalizer'
 import {
   FETCH_TICKET_DATA,
+  FETCH_PROFILE_DATA,
   UPDATE_IS_FETCHING,
   UPDATE_NEW_TICKET,
   SET_FILTER,
   FETCH_COMMENT_DATA,
   UPDATE_IS_FETCHING_COMMENT,
   UPDATE_COMMENT_DATA,
-  UPDATE_SCORE
+  UPDATE_SCORE,
+  UPDATE_IS_FETCHING_PROFILE
 } from './constants'
 
 const initialState = fromJS({
@@ -16,15 +18,21 @@ const initialState = fromJS({
   filters: ["todo", "inprogress"],
   isFetching: false,
   isFetchingComment: false,
-  commentData: {}
+  isFetchingProfile: false,
+  commentData: {},
+  profileData: {}
 })
 
 function ticketDataReducer(state = initialState, action) {
   switch (action.type) {
     case FETCH_TICKET_DATA:
       return state.set('data', fromJS(action.payload))
+    case FETCH_PROFILE_DATA:
+      return state.set('profileData', fromJS(action.payload))
     case UPDATE_IS_FETCHING:
       return state.set('isFetching', action.status)
+    case UPDATE_IS_FETCHING_PROFILE:
+      return state.set('isFetchingProfile', action.status)
     case UPDATE_NEW_TICKET:
       return state.setIn(['data', 'tickets'], fromJS(action.payload))
     case SET_FILTER:
