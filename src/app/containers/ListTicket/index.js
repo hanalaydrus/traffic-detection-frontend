bf8d14259691cfe2238947fc615ef37a2a605020
probeadd <prabowo.satrioadi@gmail.com>
2017-06-16 14:38:03 +0700

    Add refresh button, add ticket number and ticket name, normal text

--- src/app/containers/ListTicket/index.js
+++ src/app/containers/ListTicket/index.js
@@ -21,6 +21,7 @@ import {
   MenuItem,
   Drawer,
   RaisedButton,
+  FlatButton,
   Card,
   CardHeader,
   CardText,
@@ -103,6 +104,10 @@ class ListTicket extends Component {
     this.setState({
       valueMarkdown: RichTextEditor.createEmptyValue()
     })
+  }
+
+  handleRefreshComment = (projectName, ticketNumber) => {
+    this.props.fetchCommentData(projectName,ticketNumber)
   }
 
   labelColor = (labelName) => {
@@ -283,10 +288,10 @@ class ListTicket extends Component {
             {this.props.isFetchingComment ?
               (<Loader type="line-scale" active />) :
               (this.props.commentData.comments && this.props.commentData.comments.map( (row, index) => (
-                <Card style={{maxWidth: '96%'}}>
+                <Card style={{padding: 4}}>
                   <div className={"card_header"}>
                     <CardHeader
-                      title={row.user.login}
+                      title={row.user.login + " commented on #" + this.props.commentData.number + " " + this.props.commentData.title}
                       subtitle={moment(row.created_at, ["YYYY", moment.ISO_8601]).format("MMMM Do YYYY hh:mm")}
                       avatar={row.user.avatar_url}
                       titleColor='#000'
@@ -295,10 +300,11 @@ class ListTicket extends Component {
                   </div>               
                   <CardText>
                     <div className={ "cardTextContainer" }>
-                      <div dangerouslySetInnerHTML={{__html:marked(row.body)}}/>                  
+                      <div dangerouslySetInnerHTML={{__html:marked(row.body)}}/>
                     </div>
                   </CardText>
-                </Card>)))}
+                </Card>
+                )))}
             </div>
             <div className={"markdown_editor"} >
               <div className={"markdown_editor_detail"}>
@@ -308,6 +314,15 @@ class ListTicket extends Component {
                 />
               </div>
               <div className={"markdown_button_submit"} >
+                <FlatButton 
+                  label="Refresh" 
+                  primary={true}
+                  onClick={
+                    () => this.handleRefreshComment(
+                      this.state.selectedProjectName,
+                      this.state.selectedTicketNumber
+                  )}
+                />
                 <RaisedButton
                   backgroundColor="#f9bb00"
                   labelColor="#000"
