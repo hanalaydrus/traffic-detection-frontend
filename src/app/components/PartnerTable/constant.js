7127c66c5a9fd8fd7547a9404a546e65d7f9d3a0
ajilantang <ajilantang@gmail.com>
2017-06-21 05:43:19 +0700

    #44 fix bugs for update tenary

--- src/app/components/PartnerTable/constant.js
+++ src/app/components/PartnerTable/constant.js
@@ -1,32 +1,31 @@
- const data =     
-    {
-        "succes": true,
-        "data": [
-            {
-                "id": 1,
-                "name": "Bandung",
-                "address": "Jl. Ir. H. Djuanda 84, Bandung",
-                "phone": "081234567890",
-                "city_id": 34,
-                "city": {
-                    "id": 34,
-                    "name": "Bandung"
-                },
-                "description": ""
-            },
-            {
-                "id": 2,
-                "name": "Surabaya",
-                "address": "Jl. Ahmad Yani 100, Surabaya",
-                "phone": "081234567890",
-                "city_id": 12,
-                "city": {
-                    "id": 12,
-                    "name": "Suarabaya"
-                },
-                "description": ""
-            },
-        ]
-    }
+const data = {
+	succes: true,
+	data: [
+		{
+			id: 1,
+			name: 'Bandung',
+			address: 'Jl. Ir. H. Djuanda 84, Bandung',
+			phone: '081234567890',
+			city_id: 34,
+			city: {
+				id: 34,
+				name: 'Bandung'
+			},
+			description: ''
+		},
+		{
+			id: 2,
+			name: 'Surabaya',
+			address: 'Jl. Ahmad Yani 100, Surabaya',
+			phone: '081234567890',
+			city_id: 12,
+			city: {
+				id: 12,
+				name: 'Suarabaya'
+			},
+			description: ''
+		}
+	]
+};
 
-export default data
+export default data;
