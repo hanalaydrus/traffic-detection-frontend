a41552821795a7090dffdaa1d1632ce179aed8fe
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-06-28 14:55:08 +0700

    make redux for API campuses

---
+++ src/app/containers/CampusListTable/actions.js
@@ -0,0 +1,91 @@
+import {
+  refactoryAxios
+} from '../../../helpers'
+
+import {
+  UPDATE_IS_FETCHING_CAMPUSES,
+  FETCH_CAMPUSES_DATA,
+  SUBMIT_DATA_CAMPUSES,
+  IS_UPDATE_DATA_CAMPUSES,
+  DELETE_DATA_CAMPUSES,
+  UPDATE_DATA_CAMPUSES
+} from './constants'
+
+import { TOKEN } from '../../../constants'
+
+export function updateIsFetchingCampuses(status) {
+  return {
+    type: UPDATE_IS_FETCHING_CAMPUSES,
+    status
+  }
+}
+export function fetchCampusesData() {
+ return (dispatch) => {
+   // Set fetching to true
+   dispatch(updateIsFetchingCampuses(true))
+   // Make the request for contacts
+   refactoryAxios.get(`/api/campuses`, {
+     headers: {
+       Accept: "aplication/json",
+       Authorization: `Bearer ${TOKEN()}`
+    }
+   }).then( (response) => {
+     // Load the timeline data data into the reducer
+     dispatch({
+       type: FETCH_CAMPUSES_DATA,
+       payload: response.data.data
+     })
+     // Set fetching to false
+     dispatch(updateIsFetchingCampuses(false))
+   })
+ }
+}
+export function submitCampusesData (name, address, city_id, phone, description) {
+  return (dispatch, getState) => {
+    refactoryAxios.post(`/api/campuses`, ({
+        name, address, city_id, phone, description
+    }), {
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${TOKEN()}`
+      }
+    }).then((response) => {
+      dispatch({
+        type: SUBMIT_DATA_CAMPUSES,
+        payload: response.data.data
+      })
+    }).catch(err => err);
+  }
+}
+export function updateCampusesData (id, name, address, city_id, phone, description) {
+  return (dispatch, getState) => {
+    refactoryAxios.patch(`/api/campuses/${id}`, ({
+        name, address, city_id, phone, description
+    }), {
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${TOKEN()}`
+      }
+    }).then((response) => {
+      dispatch({
+        type: UPDATE_DATA_CAMPUSES,
+        payload: response.data.data
+      })
+    }).catch(err => err);
+  }
+}
+export function deleteCampusesData (id) {
+  return (dispatch, getState) => {
+    refactoryAxios.delete(`/api/campuses/${id}`, {
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${TOKEN()}`
+      }
+    }).then((response) => {
+      dispatch({
+        type: DELETE_DATA_CAMPUSES,
+        payload: response.data.data
+      })
+    }).catch(err => err);
+  }
+}
---
+++ src/app/containers/CampusListTable/reducer.js
@@ -0,0 +1,37 @@
+import { fromJS } from 'immutable';
+import normalize from 'jsonapi-normalizer'
+import {
+  UPDATE_IS_FETCHING_CAMPUSES,
+  FETCH_CAMPUSES_DATA,
+  SUBMIT_DATA_CAMPUSES,
+  IS_UPDATE_DATA_CAMPUSES,
+  DELETE_DATA_CAMPUSES,
+  UPDATE_DATA_CAMPUSES
+} from './constants'
+
+const initialState = fromJS({
+  isFetchingCampuses: false,
+  campusesData: [],
+  submitCampuses: {},
+  deleteCampuses: {},
+  updateCampuses: {},
+})
+
+function campusesDataReducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_CAMPUSES_DATA:
+      return state.set('campusesData', fromJS(action.payload))
+    case FETCH_CAMPUSES_DATA:
+      return state.set('submitCampuses', fromJS(action.payload))
+    case UPDATE_IS_FETCHING_CAMPUSES:
+      return state.set('isFetchingCampuses', action.status)
+    case DELETE_DATA_CAMPUSES:
+      return state.set('deleteCampuses', action.status)
+    case UPDATE_DATA_CAMPUSES:
+      return state.set('updateCampuses', action.status)
+    default:
+      return state
+  }
+}
+
+export default campusesDataReducer
